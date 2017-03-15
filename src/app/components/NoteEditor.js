f394266b2b68bdc4e104459ea714bcee893c32dc
Shahin Zarrabi <shahin.zarrabi@klarna.com>
2017-03-15 16:10:34 +0100

    💄 Start styling the editor area

--- src/app/components/NoteEditor.js
+++ src/app/components/NoteEditor.js
@@ -1,11 +1,40 @@
 import React, { PropTypes } from 'react';
 import moment from 'moment';
+import { StyleSheet, css } from 'aphrodite';
 
 const propTypes = {
   note: PropTypes.object.isRequired,
   onContentChange: PropTypes.func.isRequired,
   focusTitle: PropTypes.bool,
 };
+
+const styles = StyleSheet.create({
+  editor: {
+    display: 'flex',
+    width: '100%',
+    flexDirection: 'column',
+    fontFamily: 'PT Sans',
+    color: 'rgb(77, 88, 101)',
+  },
+  timestamp: {
+    display: 'flex',
+    justifyContent: 'center',
+    fontSize: '12px',
+  },
+  title: {
+    border: 0,
+    outline: 'none',
+    fontSize: '32px',
+    marginBottom: '16px',
+  },
+  content: {
+    border: 0,
+    fontSize: '16px',
+    outline: 'none',
+    lineHeight: '150%',
+    flex: '1',
+  },
+});
 
 const NoteEditor = ({ note, onContentChange, focusTitle }) => {
   let contentEditor;
@@ -23,15 +52,22 @@ const NoteEditor = ({ note, onContentChange, focusTitle }) => {
   );
 
   return (
-    <div key={note.id}>
-      { updatedAt }
+    <div
+      className={css(styles.editor)}
+      key={note.id}
+    >
+      <div className={css(styles.timestamp)}>
+        { updatedAt }
+      </div>
       <input
+        className={css(styles.title)}
         defaultValue={note.title}
         onChange={handleContentChange}
         ref={(input) => { titleEditor = input; }}
         autoFocus={focusTitle}
       />
       <textarea
+        className={css(styles.content)}
         defaultValue={note.content}
         onChange={handleContentChange}
         ref={(textarea) => { contentEditor = textarea; }}
