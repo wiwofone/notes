ab97ea1fea08e13cdc482897979d1654faa98e58
Shahin Zarrabi <shahin.zarrabi@klarna.com>
2017-03-17 11:38:10 +0100

    💩 Disable Redux devtools for now

--- src/app/store.js
+++ src/app/store.js
@@ -6,7 +6,7 @@ import rootReducer from './reducers';
 const enhancer = compose(
   persistState(),
   /* eslint-disable no-underscore-dangle */
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   /* eslint-enable */
 );
 
